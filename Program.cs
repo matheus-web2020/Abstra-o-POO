b044a1294896bfc70f3049e0f591439144a928e2
Matheus <matheus.fusca.lima@outlook.com>
2020-06-02 14:41:12 -0300

    Iniciando

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstraçãoPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
